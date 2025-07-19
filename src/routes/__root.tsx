36ae572ebdaf909a78b3d51d2ee9bfeef4a06d9e
arthurazevedods <arthurazevedods@gmail.com>
2025-07-19 19:41:51 -0300

    dev tools & links root localhost only

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,26 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
+const isLocalhost = typeof window !== "undefined" && window.location.hostname === "localhost";
+
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/colors" className="[&.active]:font-bold">
-          Colors
-        </Link>
-      </div>
-      <hr />
+      {isLocalhost && (
+        <>
+          <div className="p-2 flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>{' '}
+            <Link to="/colors" className="[&.active]:font-bold">
+              Colors
+            </Link>
+          </div>
+          <hr />
+          <TanStackRouterDevtools />
+        </>
+      )}
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
 })
